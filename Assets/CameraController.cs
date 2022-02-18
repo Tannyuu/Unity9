023b0ea0b2f999785607b70303372f9b589d19c9
hogehoge <hogehoge@gmail.com>
2022-02-18 15:12:25 +0900

    Unity9_31まで作成

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform player;
+
+	void LateUpdate()
+	{
+		/*
+		transform.position = player.position + (-player.forward * 3.0f) + (player.up * 1.0f);
+		transform.LookAt(player.position + Vector3.up);
+		*/
+
+
+		transform.position =
+			Vector3.Lerp(transform.position,
+			player.position + (-player.forward * 3.0f)
+			+ (player.up * 1f), Time.deltaTime * 10f);
+
+		transform.LookAt(player.position + Vector3.up);//Lookat　z軸を向けるメソッド  Vector3.up　Unityちゃんの中心からy軸+1
+	}
+}
